e0a3e776a1154abcd40391066dc05a7d497aee08
João Medeiros <joaoserj@gmail.com>
2019-09-09 19:30:17 -0300

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -3,84 +3,150 @@ import VueRouter from 'vue-router'
 //import routes from './routes'
 import firebase from 'firebase'
 
-import Login from '../components/Login'
-import Registro from '../components/Registro'
-import Tarefas from '../components/Tarefas'
-import Quadro from '../components/Quadro'
-import Dash from '../components/Dash'
-import Sobre from '../components/Sobre'
-import TrocarSenha from '../components/TrocarSenha'
-
 Vue.use(VueRouter)
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.VUE_ROUTER_BASE,
-  routes: [
-    {
-      path: '*',
-      redirect: '/login',
-    },
-    {
-      path: '/',
-      redirect: '/login',
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-    },
-    {
-      path: '/registrar',
-      name: 'Registrar',
-      component: Registro,
-    },
-    {
-      path: '/sobre',
-      name: 'Sobre',
-      component: Sobre,
-    },
-    {
-      path: '/trocarsenha',
-      name: 'Trocar Senha',
-      component: TrocarSenha,
-      meta: { requerAuth: true }
-    },
-    {
-      path: '/dash',
-      name: 'Dash',
-      component: Dash,
-      meta: { requerAuth: true }
-    },
-    {
-      path: '/quadro',
-      name: 'Quadro',
-      component: Quadro,
-      meta: { requerAuth: true }
-    },
-    {
-      path: '/tarefas',
-      name: 'Tarefas',
-      component: Tarefas,
-      meta: { requerAuth: true }
-    }
-  ]
-})
+export default function ({ssrContext}) {
+  if (process.env.MODE !== 'ssr') {
+    const router = new VueRouter({
+      mode: 'history',
+      base: process.env.VUE_ROUTER_BASE,
+      routes: [
+        {
+          path: '*',
+          redirect: '/login',
+        },
+        {
+          path: '/',
+          redirect: '/login',
+        },
+        {
+          path: '/login',
+          name: 'Login',
+          component: () => import('../components/Login'),
+        },
+        {
+          path: '/registrar',
+          name: 'Registrar',
+          component: () => import('../components/Registro'),
+        },
+        {
+          path: '/sobre',
+          name: 'Sobre',
+          component: () => import('../components/Sobre'),
+        },
+        {
+          path: '/trocarsenha',
+          name: 'Trocar Senha',
+          component: () => import('../components/TrocarSenha'),
+          meta: { requerAuth: true }
+        },
+        {
+          path: '/dash',
+          name: 'Dash',
+          component: () => import('../components/Dash'),
+          meta: { requerAuth: true }
+        },
+        {
+          path: '/quadro',
+          name: 'Quadro',
+          component: () => import('../components/Quadro'),
+          meta: { requerAuth: true }
+        },
+        {
+          path: '/tarefas',
+          name: 'Tarefas',
+          component: () => import('../components/Tarefas'),
+          meta: { requerAuth: true }
+        }
+      ]
+    })
+    
+    router.beforeEach((to, from, next) => {
+      let usuario = firebase.auth().currentUser;
+      let autorizacao = to.matched.some(record => record.meta.requerAuth);
+    
+      if (autorizacao && !usuario) {
+        next('login')
+      } else if (autorizacao && usuario) {
+        next();
+      }
+    
+      else {
+        next();
+      }
+    })
 
-router.beforeEach((to, from, next) => {
-  let usuario = firebase.auth().currentUser;
-  let autorizacao = to.matched.some(record => record.meta.requerAuth);
+    return router;
+  } else {
+    const router = new VueRouter({
+      mode: 'history',
+      base: process.env.VUE_ROUTER_BASE,
+      routes: [
+        {
+          path: '*',
+          redirect: '/login',
+        },
+        {
+          path: '/',
+          redirect: '/login',
+        },
+        {
+          path: '/login',
+          name: 'Login',
+          component: () => import('../components/Login'),
+        },
+        {
+          path: '/registrar',
+          name: 'Registrar',
+          component: () => import('../components/Registro'),
+        },
+        {
+          path: '/sobre',
+          name: 'Sobre',
+          component: () => import('../components/Sobre'),
+        },
+        {
+          path: '/trocarsenha',
+          name: 'Trocar Senha',
+          component: () => import('../components/TrocarSenha'),
+          meta: { requerAuth: true }
+        },
+        {
+          path: '/dash',
+          name: 'Dash',
+          component: () => import('../components/Dash'),
+          meta: { requerAuth: true }
+        },
+        {
+          path: '/quadro',
+          name: 'Quadro',
+          component: () => import('../components/Quadro'),
+          meta: { requerAuth: true }
+        },
+        {
+          path: '/tarefas',
+          name: 'Tarefas',
+          component: () => import('../components/Tarefas'),
+          meta: { requerAuth: true }
+        }
+      ]
+    })
+    
+    router.beforeEach((to, from, next) => {
+      let usuario = firebase.auth().currentUser;
+      let autorizacao = to.matched.some(record => record.meta.requerAuth);
+    
+      if (autorizacao && !usuario) {
+        next('login')
+      } else if (autorizacao && usuario) {
+        next();
+      }
+    
+      else {
+        next();
+      }
+    })
 
-  if (autorizacao && !usuario) {
-      next('login')
-  } else if (autorizacao && usuario) {
-      next();
+    return router;
   }
-  
-  else {
-      next();
-  }
-})
-
-export default router;
-
+}
