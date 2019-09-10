e77844e2f8fcb5daa045a7ac0c4fe866f5e4c531
João Medeiros <joaoserj@gmail.com>
2019-09-10 19:25:38 -0300

    tela sobre update z

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,21 +1,53 @@
 <template>
-  <div>
-    <div>
-      <q-badge align="top" style="color: blue;">Tela Sobre</q-badge>
-    </div>
+  <div class="centralDiv q-pa-xs text-center">
+    <q-card class="text-center my-card" align-center>
+      <q-img src="../../statics/symbol2logo.png" style="height: 140px; max-width: 150px" />
+      <div class="centralDiv">
+        <q-card-section class="q-pa-md q-gutter-sm">
+          <div class="text-h6 text-bold">Sistema desenvolvido pela</div>
+          <div class="text-h6 text-bold">Symbol 2 Studio</div>
+          <div class="text-h6 text-bold">(2019 - {{ versionUpdate }})</div>
+          <div class="text-h6 text-bold">Contribuições:</div>
+          <q-btn color="black" type="a" target="_blank" href="https://github.com/joaomede">
+            <div class="row items-center no-wrap">
+              <div class="text-center">João Medeiros</div>
+            </div>
+          </q-btn>
+          
+          <q-btn color="black" type="a" target="_blank" href="https://github.com/augustoOliveira1993">
+            <div class="row items-center no-wrap" >
+              <div class="text-center">Augusto Oliveira</div>
+            </div>
+          </q-btn>
 
-    <br /><strong>Desenvolvedor do projeto:</strong>
-    <div class="q-pa-md q-gutter-sm">
-      <a href="https://github.com/joaomede"><q-btn color="blue" text-color="white" label="Joao Medeiros"/></a>
-      <a href="https://github.com/augustoOliveira1993"><q-btn color="black" text-color="white" label="Augusto Oliveira"/></a>
-    </div>
-
-    <br /><strong>Status:</strong> Em processo de Desenvolvimento.
+          <div class="text-h6">Contatos: (11) 96849-8492</div>
+          <div class="text-h6">E-mail: symbol2studio@gmail.com</div>
+        </q-card-section>
+      </div>
+      <q-card-section class="q-gutter-md">
+        <q-btn color="green" to="/dash">Volta para Página Inicial</q-btn>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      key: value
+    }
+  },
+  computed: {
+    versionUpdate() {
+      if (this.$store.getters.getUltimaVersao) {
+        return this.$store.getters.getUltimaVersao
+      } else {
+        return;
+      }
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
