d1b93b0b53a8bb476d964f354026995b387488c8
João Medeiros <joaoserj@gmail.com>
2019-09-11 11:45:16 -0300

    update quasar.conf

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,25 +1,19 @@
 // Configuration for your app
 
-module.exports = function (ctx) {
+module.exports = function(ctx) {
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
-    boot: [
-      'main.js',
-      'boot.js',
-      'notify.js'
-    ],
+    boot: ["main.js", "boot.js", "notify.js"],
 
-    css: [
-      'app.styl'
-    ],
+    css: ["app.styl"],
 
     extras: [
-      'roboto-font',
-      'material-icons', // optional, you are not bound to it
+      "roboto-font",
+      "material-icons", // optional, you are not bound to it
       // 'ionicons-v4',
       // 'mdi-v3',
-      'fontawesome-v5',
+      "fontawesome-v5"
       // 'eva-icons'
     ],
 
@@ -27,69 +21,64 @@ module.exports = function (ctx) {
       // all: true, // --- includes everything; for dev only!
 
       components: [
-        'QLayout',
-        'QHeader',
-        'QDrawer',
-        'QPageContainer',
-        'QPage',
-        'QToolbar',
-        'QToolbarTitle',
-        'QBtn',
-        'QIcon',
-        'QList',
-        'QItem',
-        'QItemSection',
-        'QItemLabel',
+        "QLayout",
+        "QHeader",
+        "QDrawer",
+        "QPageContainer",
+        "QPage",
+        "QToolbar",
+        "QToolbarTitle",
+        "QBtn",
+        "QIcon",
+        "QList",
+        "QItem",
+        "QItemSection",
+        "QItemLabel",
 
-        'QInput',
-        'QCard',
-        'QCardSection',
-        'QCardActions',
-        'QDialog',
-        'ClosePopup',
-        'QForm',
-        'QFooter',
-        'QAvatar',
-        'QScrollArea',
-        'QImg',
-        'QTabs',
-        'QTab',
-        'QRouteTab',
-        'QTabPanels',
-        'QTabPanel',
-        'QFab',
-        'QFabAction',
-        'QSeparator',
-        'QBar',
-        'QSpace',
-        'QSelect',
-        'QRadio',
-        'QTime',
-        'QDate',
-        'QPopupProxy',
-        'QTable',
-        'QTh',
-        'QTr',
-        'QTd',
-        'QCheckbox',
-        'QToggle',
-        'QSpinnerHourglass',
+        "QInput",
+        "QCard",
+        "QCardSection",
+        "QCardActions",
+        "QDialog",
+        "ClosePopup",
+        "QForm",
+        "QFooter",
+        "QAvatar",
+        "QScrollArea",
+        "QImg",
+        "QTabs",
+        "QTab",
+        "QRouteTab",
+        "QTabPanels",
+        "QTabPanel",
+        "QFab",
+        "QFabAction",
+        "QSeparator",
+        "QBar",
+        "QSpace",
+        "QSelect",
+        "QRadio",
+        "QTime",
+        "QDate",
+        "QPopupProxy",
+        "QTable",
+        "QTh",
+        "QTr",
+        "QTd",
+        "QCheckbox",
+        "QToggle",
+        "QSpinnerHourglass",
 
-        'QTimeline',
-        'QTimelineEntry',
-        'QBadge',
-        'QEditor'
+        "QTimeline",
+        "QTimelineEntry",
+        "QBadge",
+        "QEditor"
       ],
 
-      directives: [
-        'Ripple'
-      ],
+      directives: ["Ripple"],
 
       // Quasar plugins
-      plugins: [
-        'Notify',
-        'Cookies'
-      ]
+      plugins: ["Notify", "Cookies"]
 
       // iconSet: 'ionicons-v4'
       // lang: 'de' // Quasar language
@@ -99,13 +88,12 @@ module.exports = function (ctx) {
 
     build: {
       scopeHoisting: true,
-      vueRouterMode: 'history',
+      vueRouterMode: "history",
       // vueCompiler: true,
       // gzip: true,
       // analyze: true,
       // extractCSS: false,
-      extendWebpack(cfg) {
-      }
+      extendWebpack(cfg) {}
     },
 
     devServer: {
@@ -125,38 +113,38 @@ module.exports = function (ctx) {
       // workboxPluginMode: 'InjectManifest',
       // workboxOptions: {}, // only for NON InjectManifest
       manifest: {
-         name: 'CheckList Pessoal',
-        // short_name: 'Quasar-PWA',
-        description: 'Criado para organização',
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        name: "CheckList Pessoal",
+        short_name: 'CheckList Pessoal',
+        description: "Criado para organização",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#ffffff",
+        theme_color: "#027be3",
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
+            src: "statics/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
+            src: "statics/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
+            src: "statics/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
+            src: "statics/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
+            src: "statics/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       }
@@ -177,22 +165,19 @@ module.exports = function (ctx) {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Window only
         // win32metadata: { ... }
       },
 
       builder: {
         // https://www.electron.build/configuration/configuration
-
         // appId: 'quasar-app'
       }
     }
-  }
-}
+  };
+};
