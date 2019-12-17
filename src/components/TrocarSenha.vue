05bd6e2b8a4b145913ce23b555d4a80ab962be4d
ultrafiber <joaoserj@gmail.com>
2019-12-17 10:30:46 -0300

    p- lint fix TrocarSenha.vue

--- src/components/TrocarSenha.vue
+++ src/components/TrocarSenha.vue
@@ -3,18 +3,41 @@
     <q-form>
       <q-card class="my-card">
         <q-card-section>
-          <div class="text-h6">Alterar senha</div>
+          <div class="text-h6">
+            Alterar senha
+          </div>
         </q-card-section>
 
         <q-card-section>
           <q-form class="q-gutter-md">
-            <q-input v-model="senha1" type="password" :rules="senhaRules" label="Senha" required filled lazy-rules></q-input>
-            <q-input v-model="senha2" type="password" :rules="senhaRules" label="Repita a senha" required filled lazy-rules />
+            <q-input
+              v-model="senha1"
+              type="password"
+              :rules="senhaRules"
+              label="Senha"
+              required
+              filled
+              lazy-rules
+            />
+            <q-input
+              v-model="senha2"
+              type="password"
+              :rules="senhaRules"
+              label="Repita a senha"
+              required
+              filled
+              lazy-rules
+            />
           </q-form>
         </q-card-section>
 
         <q-card-section>
-          <q-btn color="primary" @click.stop="dialog = true">Alterar Senha</q-btn>
+          <q-btn
+            color="primary"
+            @click.stop="dialog = true"
+          >
+            Alterar Senha
+          </q-btn>
         </q-card-section>
       </q-card>
     </q-form>
@@ -23,15 +46,27 @@
     <q-dialog v-model="dialog">
       <q-card>
         <q-card-section>
-          <div class="text-h6">Desenha realmente alterar a senha?</div>
+          <div class="text-h6">
+            Desenha realmente alterar a senha?
+          </div>
         </q-card-section>
 
         <q-card-section>
-          <q-btn color="primary" @click="trocarSenha">Trocar Senha</q-btn>
+          <q-btn
+            color="primary"
+            @click="trocarSenha"
+          >
+            Trocar Senha
+          </q-btn>
         </q-card-section>
 
         <q-card-section>
-          <q-btn color="primary" @click.stop="dialog = false">Cancelar</q-btn>
+          <q-btn
+            color="primary"
+            @click.stop="dialog = false"
+          >
+            Cancelar
+          </q-btn>
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -40,11 +75,18 @@
     <q-dialog v-model="popMensagem">
       <q-card>
         <q-card-section>
-          <div class="text-h6">{{ Mensagem }}</div>
+          <div class="text-h6">
+            {{ Mensagem }}
+          </div>
         </q-card-section>
 
         <q-card-section>
-          <q-btn color="primary" @click.stop="popMensagem = false">Ok!</q-btn>
+          <q-btn
+            color="primary"
+            @click.stop="popMensagem = false"
+          >
+            Ok!
+          </q-btn>
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -52,60 +94,59 @@
 </template>
 
 <script>
-import firebase from "firebase";
-import { timestamp } from "../boot/main";
+import firebase from 'firebase'
 
 export default {
   data: () => ({
     valid: false,
     dialog: false,
-    Mensagem: "",
+    Mensagem: '',
     popMensagem: false,
-    senha1: "",
-    senha2: "",
-    senhaRules: [v => !!v || "Senha é requerida", v => v.length >= 6 || "Precisa ter mais de 6 dígitos"]
+    senha1: '',
+    senha2: '',
+    senhaRules: [v => !!v || 'Senha é requerida', v => v.length >= 6 || 'Precisa ter mais de 6 dígitos']
   }),
-  methods: {
-    setUser: function() {
-      this.$store.dispatch("setUser");
-    },
-    trocarSenha() {
-      const user = firebase.auth().currentUser;
-      if (this.senha1 === this.senha2) {
-        this.user
-          .updatePassword(this.senha1)
-          .then(() => {
-            this.$notifiy("senha atualizada", "green");
-          })
-          .catch(() => {
-            this.$notifiy("erro ao atualizar senha", "red");
-          });
-        this.senha1 = "";
-        this.senha2 = "";
-        this.dialog = false;
-        this.Mensagem = "A senha foi alterada com sucesso!";
-        this.popMensagem = true;
+
+  computed: {
+    user () {
+      if (this.$store.getters.getUser != null) {
+        return this.$store.getters.getUser
       } else {
-        this.Mensagem = "A alteração falhou!";
-        this.dialog = false;
-        this.popMensagem = true;
+        return { uid: null, email: null }
       }
     }
   },
-  created() {
-    this.setUser();
+  created () {
+    this.setUser()
   },
-
-  computed: {
-    user() {
-      if (this.$store.getters.getUser != null) {
-        return this.$store.getters.getUser;
+  methods: {
+    setUser: function () {
+      this.$store.dispatch('setUser')
+    },
+    trocarSenha () {
+      const user = firebase.auth().currentUser
+      if (this.senha1 === this.senha2) {
+        user
+          .updatePassword(this.senha1)
+          .then(() => {
+            this.$notifiy('senha atualizada', 'green')
+          })
+          .catch(() => {
+            this.$notifiy('erro ao atualizar senha', 'red')
+          })
+        this.senha1 = ''
+        this.senha2 = ''
+        this.dialog = false
+        this.Mensagem = 'A senha foi alterada com sucesso!'
+        this.popMensagem = true
       } else {
-        return { uid: null, email: null };
+        this.Mensagem = 'A alteração falhou!'
+        this.dialog = false
+        this.popMensagem = true
       }
     }
   }
-};
+}
 </script>
 
 <style scoped>
@@ -120,4 +161,3 @@ form > * {
   display: block;
 }
 </style>
-/* eslint-disable */
