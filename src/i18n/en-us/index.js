f56931cc43623f85cb81e31248ea043dbcc1029c
Augusto Silva <gutim160@gmail.com>
2019-12-19 20:16:27 -0300

    Tradução de Português para Inglês v1.0

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -1,14 +1,86 @@
 export default {
   // APP
-  title: 'CheckList Pessoal',
-  menuDrawer: [
-    {
-      home: 'Tela Inicial',
-      updates: 'Atualizações',
-      changePassword: 'Trocar Senha',
-      about: 'Sobre'
+  app: {
+    title: 'CheckList Pessoal',
+    menuDrawer: [
+      {
+        home: 'Home screen',
+        updates: 'Updatess',
+        changePassword: 'Change Password',
+        about: 'About'
+      }
+    ],
+    footerMessage: 'Get organized',
+    newFolderAdded: 'New Folder Added'
+  },
+  // Folder
+  folder: {
+    welcome: 'Welcome to the App',
+    title: 'Select your home folder'
+  },
+  board: {
+    title: 'Select board'
+  },
+  task: {
+    title: 'Tasks'
+  },
+  dialogs: {
+    addNewFolder: 'Add a folder',
+    addNewBoard: 'Add a frame',
+    addNewTask: 'Add a Task',
+    finishTaskNoteTitle: 'Concluding Note:',
+    description: 'Description:',
+    editFolder: 'Edit folder name',
+    editBoard: 'Edit Frame Name',
+    editTask: 'Edit Task',
+    questionDelete: 'Do you really want to delete?',
+    questionFinish: 'Do you really want to complete task?',
+    questionRestore: 'Do you want to restore this task:',
+    questionForAddNotes: 'Do you want to add any concluding notes?'
+  },
+  alert: {
+    error: {
+      errorTryingToAdd: 'Error trying to add',
+      errorTryingToRemove: 'Error trying to remove',
+      errorTryingToUpdated: 'Error trying to update',
+      errorTryingToLoading: 'Error trying to load',
+      //
+      errorTryingToRestore: 'Error trying to restore this task',
+      errorTryingToFinish: 'Error trying to finish this task'
+    },
+    sucess: {
+      addedFolder: 'New folder added successfully',
+      addedBoard: 'New frame added successfully',
+      addedTask: 'New task added successfully',
+      //
+      updatedFolder: 'Folder updated successfully',
+      updatedBoard: 'Frame updated successfully',
+      updatedTask: 'Task updated successfully',
+      //
+      removedFolder: 'Folder successfully removed',
+      removedBoard: 'Frame successfully removed',
+      removedTask: 'Task successfully removed',
+      //
+      finishTask: 'Task completed successfully',
+      restoreTask: 'Task has been successfully restored'
     }
-  ],
-  footerMessage: 'Get organized',
-  newFolderAdded: 'Nova Pasta Adicionada'
+  },
+  geral: {
+    back: 'Come back',
+    save: 'To save',
+    yes: 'yes',
+    delete: 'Delete',
+    edit: 'To edit'
+  },
+  register: {
+    title: 'Sign up',
+    questionName: 'What\'s your name?',
+    password: 'password',
+    labelRepeatPassword: 'Repeat your password',
+    btnRegister: 'Sign up',
+    sucessRegister: 'Successful registration',
+    failRegister: 'Error trying to create user',
+    differentPasswords: 'Passwords are different'
+  }
 }
+
