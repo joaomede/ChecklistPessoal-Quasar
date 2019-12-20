235324b51d93b808b7bdb0adacbcb0680606027b
Joao Medeiros <joaoserj@gmail.com>
2019-12-20 10:23:07 -0300

    p- created component fab back page desktop

---
+++ src/components/button/FabBackPageDesktop.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-btn
+    v-if="$q.platform.is.desktop"
+    round
+    color="orange darken-2"
+    class="fixed fabBack"
+    @click.stop="$router.go(-1)"
+  >
+    <q-icon name="fas fa-arrow-left" />
+  </q-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    eventClick () {
+      this.$emit('eventClick')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
