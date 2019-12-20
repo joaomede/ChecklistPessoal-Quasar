7ab04fee19eeee3c3e6242f1259f7fcd6758c6e1
Joao Medeiros <joaoserj@gmail.com>
2019-12-20 10:22:57 -0300

    p- created component fab back page mobile

---
+++ src/components/button/FabBackPageMobile.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-btn
+    v-if="$q.platform.is.mobile"
+    round
+    color="orange darken-2"
+    class="fixed fabLeft"
+    @click.stop="eventClick()"
+  >
+    <q-icon name="fas fa-arrow-left" />
+  </q-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    eventClick () {
+      this.$emit('eventClick')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
