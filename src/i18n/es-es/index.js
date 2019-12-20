f9a10f41f1f4c8de7da5699295348dffabaae03b
augustoOliveira1993 <gutim160@gmail.com>
2019-12-20 09:16:45 -0300

    Finalização de tradução es-ES - Espanhol

---
+++ src/i18n/es-es/index.js
@@ -0,0 +1,85 @@
+export default {
+    // APP
+    app: {
+      title: 'Lista de verificación persona',
+      menuDrawer: {
+        home: 'Pantalla de inicio',
+        updates: 'Actualizaciones',
+        changePassword: 'Cambiar contraseña',
+        about: 'Acerca de'
+  
+      },
+      footerMessage: 'Organízate',
+      newFolderAdded: 'Nueva carpeta agregada'
+    },
+    // Carpeta
+    folder: {
+      welcome: 'Bienvenido a la aplicación',
+      title: 'Seleccione su carpeta de inicio'
+    },
+    board: {
+      title: 'Seleccionar tablero'
+    },
+    task: {
+      title: 'Tareas'
+    },
+    dialogs: {
+      addNewFolder: 'Agregar una carpeta',
+      addNewBoard: 'Agrega una alfombra',
+      addNewTask: 'Agregar una tarea',
+      finishTaskNoteTitle: 'Nota final:',
+      description: 'Descripción::',
+      editFolder: 'Editar nombre de carpeta',
+      editBoard: 'Editar nombre del marco',
+      editTask: 'Editar tarea',
+      questionDelete: '¿De verdad quieres eliminar?',
+      questionFinish: '¿De verdad quieres completar la tarea?',
+      questionRestore: '¿Quieres restaurar esta tarea?',
+      questionForAddNotes: '¿Quieres agregar alguna nota final?'
+    },
+    alert: {
+      error: {
+        errorTryingToAdd: 'Error al intentar agregar',
+        errorTryingToRemove: 'Error al intentar eliminar',
+        errorTryingToUpdated: 'Error al intentar actualizar',
+        errorTryingToLoading: 'Error al intentar cargar',
+        //
+        errorTryingToRestore: 'Error al intentar restaurar esta tarea',
+        errorTryingToFinish: 'Error al intentar finalizar esta tarea'
+      },
+      sucess: {
+        addedFolder: 'Nueva carpeta agregada exitosamente',
+        addedBoard: 'Nuevo marco agregado con éxito',
+        addedTask: 'Nueva tarea agregada exitosamente',
+        //
+        updatedFolder: 'Carpeta actualizada correctamente',
+        updatedBoard: 'Marco actualizado con éxito',
+        updatedTask: 'Tarea actualizada con éxito',
+        //
+        removedFolder: 'Carpeta eliminada correctamente',
+        removedBoard: 'Marco eliminado con éxito',
+        removedTask: 'Tarea eliminada con éxito',
+        //
+        finishTask: 'Tarea completada con éxito',
+        restoreTask: 'La tarea ha sido restaurada exitosamente'
+      }
+    },
+    geral: {
+      back: 'Volver',
+      save: 'Guardar',
+      yes: 'Si',
+      delete: 'Eliminar',
+      edit: 'Editar'
+    },
+    register: {
+      title: 'Registrarse',
+      questionName: '¿Cuál es tu nombre?',
+      password: 'Contraseña',
+      labelRepeatPassword: 'Repite tu contraseña',
+      btnRegister: 'Registrarse',
+      sucessRegister: 'Registro exitoso',
+      failRegister: 'Error al intentar crear usuario',
+      differentPasswords: 'Las contraseñas son diferentes'
+    }
+  }
+  
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,9 @@
 import enUS from './en-us'
 import ptBR from './pt-br'
+import esES from './es-es'
 
 export default {
   'pt-br': ptBR,
-  'en-us': enUS
+  'en-us': enUS,
+  'es-es': esES
 }
